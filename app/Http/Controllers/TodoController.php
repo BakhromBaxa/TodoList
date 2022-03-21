409719b2ec6933094233b498a870c606a6f7870f
BakhromBaxa <bahromb094@gmail.com>
2022-03-21 19:19:03 +0500

    First_project

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo; 
+
+class TodoController extends Controller
+{
+    public function index(){
+
+        $todo = Todo::all();
+        return view('index')->with('todos', $todo);
+    
+    }
+    public function create(){
+        return view('create');
+    }
+    public function details(Todo $todo){
+    
+        return view('details')->with('todos', $todo);
+    
+    }
+    
+    public function edit(Todo $todo){
+    
+        return view('edit')->with('todos',$todo);
+    }
+    public function update($id)
+    {
+        $data = request()->all();
+
+        $todo = Todo::find($id);
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->update();
+        echo ('<div class="alert alert-primary" role="alert">
+            <strong>Updated!</strong>
+        </div>'
+        );
+        return view('edit')->with('todos',$todo);
+    }
+    
+    public function delete(Todo $todo){
+
+        $todo->delete();
+
+        return redirect('/');
+
+    }
+    public function store(){
+
+        $data = request()->all();
+
+        $todo = new Todo();
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->save();
+
+        session()->flash('success', 'Todo created succesfully');
+
+        return redirect('/');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use illuminate\Routing\Redirector;
+use App\Http\Controllers\TodoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/create', function () {
+    return view('create');
+});
+
+Route::get('/details', function () {
+    return view('details');
+});
+
+Route::get('/edit', function () {
+    return view('edit');
+});
+
+Route::get('/', [TodoController::class, 'index']);
+
+Route::get('create', [TodoController::class, 'create']);
+
+Route::get('details', [TodoController::class, 'details']);
+
+Route::get('details/{todo}', [TodoController::class, 'details']);
+
+Route::get('edit', [TodoController::class, 'edit']);
+
+Route::get('edit/{todo}', [TodoController::class, 'edit']);
+
+Route::post('update', [TodoController::class, 'update']);
+
+Route::post('update/{todo}', [TodoController::class, 'update']);
+
+Route::get('delete', [TodoController::class, 'delete']);
+
+Route::get('delete/{todo}', [TodoController::class, 'delete']);
+
+Route::post('store-data', [TodoController::class, 'store']);
+
